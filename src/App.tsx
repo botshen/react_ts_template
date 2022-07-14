515ae65ae65458a0a11ef537c57d8ff001258cfc
shenxin <wb_shenxin@meituan.com>
2022-07-14 19:35:41 +0800

    chore: init project with React+Ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from 'components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Hello World" />
+    </div>
+  );
+}
+
+export default App;
